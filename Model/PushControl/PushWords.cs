3eceb31f767a45dc7363e6e4872a0730c369a189
Uahhhhhh <hnyrarara@gmail.com>
2021-05-05 15:18:53 +0800

    背诵核心推送逻辑

---
+++ Model/PushControl/PushWords.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Uwp.Notifications;
+using ToastFish.Model.SqliteControl;
+
+namespace ToastFish.PushControl
+{
+    class PushWords
+    {
+        public static void Recitation(int number)
+        {
+            Select Query = new Select();
+            List<Word> Result = Query.GetRandomWordList(number);
+
+            //foreach(var Word in Result)
+            //{
+
+            //}
+
+            // 基本单词
+            new ToastContentBuilder()
+                .AddText("cancel  ('kænsl')\nvt.取消，撤销；删去")
+                .AddText("They are called ‘Supertrams’, the implication being that (= which is meant to suggest that ) they are more advanced than earlier models.\n它们被称作“超级电车”，这是暗指它们比先前的型号更先进。")
+                .Show();
+
+            ////选择题
+            //new ToastContentBuilder()
+            //.AddText("question")
+            //.AddText("As we can no longer wait for the delivery of our order, we have to _______ it.", hintMaxLines: 2)
+            //.AddButton(new ToastButton()
+            //    .SetContent("A.postpone")
+            //    .AddArgument("action", "reply")
+            //    .SetBackgroundActivation())
+
+            //.AddButton(new ToastButton()
+            //    .SetContent("B.refuse")
+            //    .AddArgument("action", "like")
+            //    .SetBackgroundActivation())
+
+            //.AddButton(new ToastButton()
+            //    .SetContent("C.delay")
+            //    .AddArgument("delay", "like")
+            //    .SetBackgroundActivation())
+
+            //.AddButton(new ToastButton()
+            //    .SetContent("D.cancel1234567890")
+            //    .AddArgument("action", "like")
+            //    .SetBackgroundActivation())
+            //.Show();
+        }
+    }
+}
---
+++ Model/SqliteControl/Select.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using Dapper;
+
+
+namespace ToastFish.Model.SqliteControl
+{
+    public class Select
+    {
+        public Select()
+        {
+            DataBase = connectToDatabase();
+        }
+
+        SQLiteConnection DataBase;
+        IEnumerable<Word> WordList;
+
+        SQLiteConnection connectToDatabase()
+        {
+            var databasePath = "Data Source=D:\\VS\\ToastFish\\Model\\SqliteControl\\inami.db;Version=3";
+            return new SQLiteConnection(databasePath);
+        }
+
+        public void SelectWordList()
+        {
+            Word temp = new Word();
+            WordList = DataBase.Query<Word>("select * from CET4_1", temp);
+        }
+
+        public List<Word> GetRandomWordList(int Number)
+        {
+            List<Word> Result = new List<Word>();
+            SelectWordList();
+            var WordArray = WordList.ToArray();
+            List<int> RandomList = new List<int>();
+            foreach(var Word in WordList)
+            {
+                if(Word.status == 0)
+                {
+                    RandomList.Add(Word.wordRank);
+                }
+            }
+
+            Random Rd = new Random();
+            for (int i = 0; i < Number; i++)
+            {
+                int Index = Rd.Next(RandomList.Count);
+                Result.Add(WordArray[Index]);
+            }
+
+            return Result;
+        }
+    }
+
+    public class Word
+    {
+        public int wordRank { get; set; }
+        public int status { get; set; }
+        public String headWord { get; set; }
+        public String usPhone { get; set; }
+        public String ukPhone { get; set; }
+        public String usSpeech { get; set; }
+        public String ukSpeech { get; set; }
+        public String tranCN { get; set; }
+        public String pos { get; set; }
+        public String tranOther { get; set; }
+        public String question { get; set; }
+        public String explain { get; set; }
+        public String rightInde { get; set; }
+        public String examType { get; set; }
+        public String choiceIndexOne { get; set; }
+        public String choiceIndexTwo { get; set; }
+        public String choiceIndexThree { get; set; }
+        public String choiceIndexFour { get; set; }
+        public String sentence { get; set; }
+        public String sentenceCN { get; set; }
+        public String phrase { get; set; }
+        public String phraseCN { get; set; }
+    }
+}
