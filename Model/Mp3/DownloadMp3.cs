cd5c58a7e6e67da7bfcf12e4584802285d7ea520
Uahh <uahh.yang@designorder.cn>
2021-05-17 21:59:59 +0800

    添加音频

---
+++ Model/Mp3/DownloadMp3.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToastFish.Model.Download
+{
+    class DownloadMp3
+    {
+        /// <summary>
+        /// http下载文件
+        /// </summary>
+        /// <param name="url">下载文件地址</param>
+        /// <param name="path">文件存放地址，包含文件名</param>
+        /// <returns></returns>
+        public bool HttpDownload(string Url, string Name)
+        {
+            string CachePath = System.IO.Directory.GetCurrentDirectory() + @"\Mp3Cache";
+            if (!System.IO.File.Exists(CachePath))
+            {
+                System.IO.Directory.CreateDirectory(CachePath);  //创建临时文件目录
+            }
+            string CacheFail = CachePath + @"\" + Name + ".mp3"; 
+            try
+            {
+                if (System.IO.File.Exists(CacheFail))
+                {
+                    System.IO.File.Move(CacheFail, CachePath);
+                }
+                FileStream fs = new FileStream(CacheFail, FileMode.Append, FileAccess.Write, FileShare.ReadWrite);
+                // 设置参数
+                HttpWebRequest request = WebRequest.Create(Url) as HttpWebRequest;
+                //发送请求并获取相应回应数据
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                //直到request.GetResponse()程序才开始向目标网页发送Post请求
+                Stream responseStream = response.GetResponseStream();
+                //创建本地文件写入流
+                //Stream stream = new FileStream(tempFile, FileMode.Create);
+                byte[] bArr = new byte[1024];
+                int size = responseStream.Read(bArr, 0, (int)bArr.Length);
+                while (size > 0)
+                {
+                    //stream.Write(bArr, 0, size);
+                    fs.Write(bArr, 0, size);
+                    size = responseStream.Read(bArr, 0, (int)bArr.Length);
+                }
+                //stream.Close();
+                fs.Close();
+                responseStream.Close();
+                //System.IO.File.Move(tempFile, path);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Model/Mp3/PlayMp3.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace ToastFish.Model.Mp3
+{
+    /// <summary>
+    /// MUSIC 的摘要说明。
+    /// </summary>
+    public class MUSIC
+    {
+        public MUSIC()
+        {
+        }
+        //定义API函数使用的字符串变量 
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+        private string Name = "";
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+        private string durLength = "";
+        [MarshalAs(UnmanagedType.LPTStr, SizeConst = 128)]
+        private string TemStr = "";
+        int ilong;
+        //定义播放状态枚举变量
+        public enum State
+        {
+            mPlaying = 1,
+            mPuase = 2,
+            mStop = 3
+        };
+        //结构变量
+        public struct structMCI
+        {
+            public bool bMut;
+            public int iDur;
+            public int iPos;
+            public int iVol;
+            public int iBal;
+            public string iName;
+            public State state;
+        };
+        public structMCI mc = new structMCI();
+        //取得播放文件属性
+        public string FileName
+        {
+            get
+            {
+                return mc.iName;
+            }
+            set
+            {
+                try
+                {
+                    TemStr = "";
+                    TemStr = TemStr.PadLeft(127, Convert.ToChar(" "));
+                    Name = Name.PadLeft(260, Convert.ToChar(" "));
+                    mc.iName = value;
+                    ilong = APIClass.GetShortPathName(mc.iName, Name, Name.Length);
+                    Name = GetCurrPath(Name);
+                    Name = "open " + Convert.ToChar(34) + Name + Convert.ToChar(34) + " alias media";
+                    ilong = APIClass.mciSendString("close all", TemStr, TemStr.Length, 0);
+                    ilong = APIClass.mciSendString(Name, TemStr, TemStr.Length, 0);
+                    ilong = APIClass.mciSendString("set media time format milliseconds", TemStr, TemStr.Length, 0);
+                    mc.state = State.mStop;
+                }
+                catch
+                {
+                }
+            }
+        }
+        //播放
+        public void play()
+        {
+            TemStr = "";
+            TemStr = TemStr.PadLeft(127, Convert.ToChar(" "));
+            APIClass.mciSendString("play media", TemStr, TemStr.Length, 0);
+            mc.state = State.mPlaying;
+        }
+        //停止
+        public void StopT()
+        {
+            TemStr = "";
+            TemStr = TemStr.PadLeft(128, Convert.ToChar(" "));
+            ilong = APIClass.mciSendString("close media", TemStr, 128, 0);
+            ilong = APIClass.mciSendString("close all", TemStr, 128, 0);
+            mc.state = State.mStop;
+        }
+        public void Puase()
+        {
+            TemStr = "";
+            TemStr = TemStr.PadLeft(128, Convert.ToChar(" "));
+            ilong = APIClass.mciSendString("pause media", TemStr, TemStr.Length, 0);
+            mc.state = State.mPuase;
+        }
+        private string GetCurrPath(string name)
+        {
+            if (name.Length < 1) return "";
+            name = name.Trim();
+            name = name.Substring(0, name.Length - 1);
+            return name;
+        }
+        //总时间
+        public int Duration
+        {
+            get
+            {
+                durLength = "";
+                durLength = durLength.PadLeft(128, Convert.ToChar(" "));
+                APIClass.mciSendString("status media length", durLength, durLength.Length, 0);
+                durLength = durLength.Trim();
+                if (durLength == "") return 0;
+                return (int)(Convert.ToDouble(durLength) / 1000f);
+            }
+        }
+        //当前时间
+        public int CurrentPosition
+        {
+            get
+            {
+                durLength = "";
+                durLength = durLength.PadLeft(128, Convert.ToChar(" "));
+                APIClass.mciSendString("status media position", durLength, durLength.Length, 0);
+                mc.iPos = (int)(Convert.ToDouble(durLength) / 1000f);
+                return mc.iPos;
+            }
+        }
+    }
+    public class APIClass
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetShortPathName(
+         string lpszLongPath,
+         string shortFile,
+         int cchBuffer
+      );
+        [DllImport("winmm.dll", EntryPoint = "mciSendString", CharSet = CharSet.Auto)]
+        public static extern int mciSendString(
+           string lpstrCommand,
+           string lpstrReturnString,
+           int uReturnLength,
+           int hwndCallback
+          );
+    }
+}
